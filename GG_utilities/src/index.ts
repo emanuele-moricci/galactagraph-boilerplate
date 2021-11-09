ae7a53e811d7ff63b0a781097cbb5f1b66144911
emanuele-moricci <emanuele.moricci@gmail.com>
2021-11-09 17:15:36 +0100

    Removed unused graphql-shield rules

--- GG_utilities/src/index.ts
+++ GG_utilities/src/index.ts
@@ -2,7 +2,6 @@ import resolvers from './resolvers';
 
 import { cryptObject, decryptObject } from './security/Hashing';
 import { verifyToken, signToken } from './security/JWT';
-import { isAuthenticated, isAdmin, alwaysAllow } from './security/Shielding';
 
 import { PaginationAndSearchArgs } from './types/QueryArgs';
 
@@ -16,8 +15,5 @@ export {
   resolvers,
   verifyToken,
   signToken,
-  isAuthenticated,
-  isAdmin,
-  alwaysAllow,
   authDirective,
 };
