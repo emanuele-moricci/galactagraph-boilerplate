756f2d47fee9768a677b50fdeb0c0abdb7be1334
emanuele-moricci <emanuele.moricci@gmail.com>
2021-11-10 11:54:53 +0100

    Fixed prismaContext-Codegen error + fixed @apollo/rover integration in the bash and package.json

--- server/bin/publish-federation.sh
+++ server/bin/publish-federation.sh
@@ -4,7 +4,7 @@ source ..
 
 # Federation Microservices start-up [START]
 
-(cd ./services/federation-auth/ ; yarn apollo:update)
+(cd ./services/federation-auth/ ; yarn add -D @apollo/rover@0.3.0 && yarn apollo:update)
 # [ADD NEW APOLLO COMMANDS ABOVE] <- DO NOT REMOVE - Needed for the generator to create micro-service commands seamlessly
 
 # Federation Microservices start-up [END]
